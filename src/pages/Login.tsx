acf944d91b7f4912f6f5085c73877e83e9a3ead7
Mattia_Minuti <mattiaminuti@fattureincloud.it>
2023-03-21 16:51:04 +0100

    Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,60 +1,59 @@
 import React, { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import axios from "axios";
 
-const LoginPage: React.FC = () => {
-    const [validated, setValidated] = useState(false);
-    const handleSubmit = (event: { currentTarget: any; preventDefault: () => void; stopPropagation: () => void; }) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        const {elements} = form;
+function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-        if (form.checkValidity()) {
-            const first_name = elements['formBasicFirstName'].value;
-            const last_name = elements['formBasicLastName'].value;
-            const email = elements['formBasicEmail'].value;
-            const password = elements['formBasicPassword'].value;
-
-            const data = { first_name, last_name, email, password };
-
-            axios.post('http://localhost:8252/auth/register', data, {
-                headers: { 'Content-Type': 'application/json' },
-            })
-                .then((response) => {
-                    console.log(response.data);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-        setValidated(true);
-
-
+    const onClick = () =>{
+        axios.post('http://localhost:8252/auth/login', {
+            email: email,
+            password: password
+        }).then((response) => {
+            localStorage.setItem('accessToken', response.data.data.token);
+        }, (error) => {
+            console.log(error);
+        });
     };
 
     return (
-        <Container className="my-5">
-        <h1 className="mb-4">Login</h1>
-            <Form onSubmit={handleSubmit}>
-    <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email"  />
-    </Form.Group>
-
-    <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password"  />
-    </Form.Group>
-
-    <Button variant="primary" type="submit">
-        Submit
-        </Button>
-        </Form>
+        <Container>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        required
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        type="password"
+                        placeholder="Enter password"
+                        pattern=".{8,}"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={onClick}>
+                    Submit
+                </Button>
+            </Form>
         </Container>
-);
-};
+    );
+}
 
 export default LoginPage;
