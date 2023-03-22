f8269802be365310cc037786c7fa4d3574c00054
Mattia_Minuti <mattiaminuti@fattureincloud.it>
2023-03-22 14:57:44 +0100

    toast

--- src/components/NavBar/AuthenticatedForm.tsx
+++ src/components/NavBar/AuthenticatedForm.tsx
@@ -4,7 +4,7 @@ import useToken from "../hooks/useToken";
 import axios from "axios";
 import {Post, User} from "../interface/types";
 import Container from "react-bootstrap/Container";
-
+import ToastContainer from 'react-bootstrap/ToastContainer'
 interface IAUthForm {
     children: any;
 }
@@ -12,8 +12,10 @@ interface IAUthForm {
 const AuthenticatedForm : React.FC<IAUthForm> = ({children}) => {
     const api_token = useToken();
     const [content, setContent] = useState("");
-    const [showA, setShowA] = useState(true);
-   const toggleShowA = () => setShowA(!showA);
+    const [showA, setShowA] = useState(false);
+    const toggleShowA = () => setShowA(!showA);
+    const [showB, setShowB] = useState(false);
+    const toggleShowB = () => setShowB(!showB);
     if (!api_token) {
         return null; // Se il token non è presente, il form non viene mostrato
     }
@@ -25,7 +27,7 @@ const AuthenticatedForm : React.FC<IAUthForm> = ({children}) => {
         }).then((response) => {
             setShowA(true)
         }, (error) => {
-            console.log(error);
+            setShowB(true)
         });
     };
 
@@ -45,13 +47,28 @@ const AuthenticatedForm : React.FC<IAUthForm> = ({children}) => {
                     Submit
                 </Button>
             </Form>
-            <Toast show={showA} onClose={toggleShowA}>
-                <Toast.Header>
-                    <strong className="me-auto">Bootstrap</strong>
-                    <small>11 mins ago</small>
-                </Toast.Header>
-                <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
-            </Toast>
+            <ToastContainer className="p-3" position={'top-start'} containerPosition={'absolute'}>
+                <Toast
+                    bg = 'success'
+                    delay={3000} autohide
+                    show={showA} onClose={toggleShowA}>
+                    <Toast.Header>
+                        <strong className="me-auto">Success!</strong>
+                        <small>just now</small>
+                    </Toast.Header>
+                    <Toast.Body>Comment added</Toast.Body>
+                </Toast>
+                <Toast
+                    bg = 'danger'
+                    delay={3000} autohide
+                    show={showB} onClose={toggleShowB}>
+                    <Toast.Header>
+                        <strong className="me-auto">Error!</strong>
+                        <small>just now</small>
+                    </Toast.Header>
+                    <Toast.Body>Something goes wrong</Toast.Body>
+                </Toast>
+            </ToastContainer>
         </Container>
     );
 }
